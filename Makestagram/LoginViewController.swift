71a908385bb037151b3147c237eef342315c45de
PorubcinJ <porubcinj@gmail.com>
2017-06-26 11:52:35 -0500

    Makestagram

---
+++ Makestagram/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Jozef Porubcin on 6/26/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
