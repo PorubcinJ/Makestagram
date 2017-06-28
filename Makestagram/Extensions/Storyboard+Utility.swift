8f8ff2f15bbec893a91c4e3eab10e3f66d35114f
PorubcinJ <porubcinj@gmail.com>
2017-06-28 10:30:38 -0500

    Finished Chapter 15

--- Makestagram/Extensions/Storyboard+Utility.swift
+++ Makestagram/Extensions/Storyboard+Utility.swift
@@ -17,9 +17,11 @@ extension UIStoryboard {
             return rawValue.capitalized
         }
     }
+	
     convenience init(type: MGType, bundle: Bundle? = nil) {
         self.init(name: type.filename, bundle: bundle)
     }
+	
     static func initialViewController(for type: MGType) -> UIViewController {
         let storyboard = UIStoryboard(type: type)
         guard let initialViewController = storyboard.instantiateInitialViewController() else {
@@ -28,4 +30,5 @@ extension UIStoryboard {
         
         return initialViewController
     }
+	
 }
